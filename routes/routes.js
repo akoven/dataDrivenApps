d8ba91dc1c7e438480597255ee9698d21f8da5c6
Attiya <adarensburg@live.com>
2022-03-31 16:50:27 -0500

    added async handler and more bootstrap template buttons

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/models');
 
-router.get('/', async (req, res, next) => {
-    try {
+const aysncHandler = (handler) =>
+ (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', aysncHandler(async (req, res) => {
         const books = await db.Book.findAll({order: [['title', 'ASC']]});
-        res.render('index', {title: 'Home', books});
-    } catch (error) {
-        next(error);
-    }
+        res.render('book-list', {title: 'Books', books});
+
+
     // // throw new Error('This is a test error!');
     // res.render('index',{title: 'Home'});
-});
+}));
 
 module.exports = router;
