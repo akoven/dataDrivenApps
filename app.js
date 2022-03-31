3a884e49cbfb487962aa720a74f767c4c5227796
Kai Seward <kairality@gmail.com>
2022-03-31 10:46:14 -0700

    added morgan

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const routes = require('./routes/routes');
+
 
 const app = express();
 app.set('view engine', 'pug');
 
+app.use(morgan('dev'));
+
 app.use(routes);
 
-const port = 8080;
+// const port = 8080;
 
-app.listen(port, () => console.log(`listening on port ${port}...`));
+// app.listen(port, () => console.log(`listening on port ${port}...`));
+
+module.exports = app;
