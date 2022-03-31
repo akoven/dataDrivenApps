1c471ff545269cf0a87d0a7a6469975a80478035
Kai Seward <kairality@gmail.com>
2022-03-31 10:57:34 -0700

    Added custom error handlers

--- app.js
+++ app.js
@@ -11,6 +11,47 @@ app.use(morgan('dev'));
 
 app.use(routes);
 
+// catch unhandled requests and forward to error handler
+app.use((req, res, next) => {
+    const error = new Error('The requested page couldn\'t be found.');
+    error.status = 404;
+    next(error);
+});
+
+//custom error handlers
+
+//error handler to log errors
+app.use((error, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        //TODO log the error to the database
+    } else {
+        console.error(error);
+    }
+    next(error);
+});
+
+// handle 404 errors
+app.use((error, req, res, next) => {
+    if (error.status === 404) {
+        res.status(404);
+        res.render('page-not-found', { title: 'Page Not Found'});
+    } else {
+        next(error);
+    }
+});
+
+// generic error handling
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+        title: 'Server Error',
+        message: isProduction ? null : error.message,
+        stack: isProduction ? null : error.stack,
+    });
+});
+
 // const port = 8080;
 
 // app.listen(port, () => console.log(`listening on port ${port}...`));
